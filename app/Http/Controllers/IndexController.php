e0838deaf63e720a2718e7b62ed511572897f782
sifat hassan <sifat27.sh22@gmail.com>
2020-03-22 11:44:58 +0600

    retrive all categrory data with relationship for sub category

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Product;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,19 @@ class IndexController extends Controller
 {
     public function index()
     {
+        //in Ascending order
+        $productsAll=Product::get();
+
+        //in dscending order
         $productsAll=Product::orderBy('id','DESC')->get();
-        return view('index')->with(compact('productsAll'));
+
+        //in random order
+        $productsAll=Product::inRandomorder()->get();
+
+        //get category and sub cetgory
+        $categories=Category::with('categories')->where(['parent_id'=>0])->get();
+       
+
+        return view('index')->with(compact('productsAll','categories'));
     }
 }
