552386742beff1fc3e0e16c6606ddeabb92e43df
sifat hassan <sifat27.sh22@gmail.com>
2020-05-15 12:44:01 +0600

    debag cart functionality

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,6 +38,11 @@ class UserController extends Controller
 
                 if(Auth::attempt(['email'=>$data['email'],'password'=>$data['password']])){
                     Session::put('frontSession',$data['email']);
+
+                    if(!empty(Session::get('session_id'))){
+                        $session_id = Session::get('session_id');
+                        DB::table('cart')->where('session_id',$session_id)->update(['user_email' => $data['email']]);
+                    }
                     return redirect('/cart');
                 }
                
@@ -68,6 +73,11 @@ class UserController extends Controller
             /*echo "<pre>"; print_r($data); die;*/
             if(Auth::attempt(['email'=>$data['email'],'password'=>$data['password']])){
                 Session::put('frontSession',$data['email']);
+
+                if(!empty(Session::get('session_id'))){
+                    $session_id = Session::get('session_id');
+                    DB::table('cart')->where('session_id',$session_id)->update(['user_email' => $data['email']]);
+                }
                 return redirect('/cart');
             }else{
                 return redirect()->back()->with('flash_message_error','Invalid Username or Password!');
