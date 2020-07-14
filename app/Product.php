c838b9911da517a0ea58e54701676ff86d447f7e
sifat hassan <sifat27.sh22@gmail.com>
2020-07-14 11:02:44 +0600

    count cart item and show in head of the page in cart icon alosocount product for indevisual ctrgory

--- app/Product.php
+++ app/Product.php
@@ -2,7 +2,10 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Session;
 
 class Product extends Model
 {
@@ -10,4 +13,23 @@ class Product extends Model
     {
         return $this->hasMany('App\ProductsAttribute','product_id');
     }
+
+    public static function cartCount(){
+    	if(Auth::check()){
+    		// User is logged in; We will use Auth
+    		$user_email = Auth::user()->email;
+    		$cartCount = DB::table('cart')->where('user_email',$user_email)->sum('quantity');
+    	}else{
+    		// User is not logged in. We will use Session
+    		$session_id = Session::get('session_id');
+    		$cartCount = DB::table('cart')->where('session_id',$session_id)->sum('quantity');
+    	}
+    	return $cartCount;
+    }
+
+    public static function productCount($cat_id){
+    	$catCount = Product::where(['category_id'=>$cat_id,'status'=>1])->count();
+    	return $catCount;
+    }
+
 }
--- resources/views/layouts/frontLayout/front_sidebar.blade.php
+++ resources/views/layouts/frontLayout/front_sidebar.blade.php
@@ -1,3 +1,5 @@
+<?php use App\Product; ?>
+
 <div class="left-sidebar">
     <h2>Category</h2>
     <div class="panel-group category-products" id="accordian"><!--category-productsr-->
@@ -17,8 +19,9 @@
                 <div class="panel-body">
                     <ul>
                         @foreach ($cat->categories as $subcat)
+                        <?php $productCount = Product::productCount($subcat->id); ?>
                         @if ($subcat->status==1)
-                        <li><a href="{{asset('/products/'.$subcat->url)}}">{{$subcat->name}}</a></li>
+                        <li><a href="{{asset('/products/'.$subcat->url)}}">{{$subcat->name}}</a> ({{ $productCount }})</li>
                         @endif
                        @endforeach
                     </ul>
